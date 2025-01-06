7ba742e860e2c6736865ee9594b2f3466ccc574c
aleanalesnik <alea.nalesnik@gmail.com>
2025-01-06 12:15:42 -0500

    Initial Commit

---
+++ Habit Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by Alea Nalesnik on 1/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habit TrackerTests/Habit_TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Habit_TrackerTests.swift
+//  Habit TrackerTests
+//
+//  Created by Alea Nalesnik on 1/6/25.
+//
+
+import Testing
+
+struct Habit_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
