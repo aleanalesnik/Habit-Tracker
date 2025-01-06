d7c6e918884325b652cea995ef699b0685d03de6
aleanalesnik <alea.nalesnik@gmail.com>
2025-01-06 16:21:54 -0500

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Alea Nalesnik on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Alea Nalesnik on 1/6/25.
+//
+
+import Testing
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
